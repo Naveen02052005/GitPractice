b86355e9f87c9a671e32cba460b35b97cca246fd
Merugu Naveen <naveenmerugu959@gmail.com>
2025-11-03 23:37:39 +0530

    Add Search schema for MongoDB using Mongoose

--- Assignment/server/models/Search.js
+++ Assignment/server/models/Search.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+const SearchSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  term: { type: String, required: true, index: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Search', SearchSchema);
 
